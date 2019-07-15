d473d62dc6a6691432a60836c88919349f60abe6
Álister Lopes Ferreira <fornecedor.VFerreira@brasilprev.com.br>
2019-07-14 23:51:32 -0300

    delete v1

--- src/app/custom-material-module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatListModule, MatCheckboxModule, MatToolbarModule, MatIconModule } from '@angular/material';
-
-@NgModule({
-  imports: [MatListModule, MatCheckboxModule, MatToolbarModule, MatIconModule],
-  exports: [MatListModule, MatCheckboxModule, MatToolbarModule, MatIconModule],
-})
-export class CustomMaterialModule { }
